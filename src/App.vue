c64efd515b038a36ee42df9d4760c743eb5e91ab
Aaron Huang <aaronhuang@snail-ares.local>
2020-12-09 22:46:07 +0800

    fix(chore): tslint

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent} from 'vue';
+import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
 
-export default defineComponent ({
-    components: {
+export default defineComponent({
+  components: {
     HelloWorld,
   },
 }
